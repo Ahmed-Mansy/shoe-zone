05c5513c239ff77961655135d8e3aea153913c0c
AndrewEssam95 <andrewessam95@gmail.com>
2025-05-02 03:14:46 +0300

    Finish nav drawer

--- frontend/sheozone_frontend/src/components/Navbar.jsx
+++ frontend/sheozone_frontend/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router";
 import Logo from "./Logo";
 import { FiSearch, FiUser, FiShoppingCart } from "react-icons/fi";
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
 import { useState } from "react";
+
+const navLinks = [
+  { id: 1, title: "men" },
+  { id: 2, title: "women" },
+  { id: 3, title: "socks" },
+  { id: 4, title: "new arrivals" },
+];
 
 const Navbar = () => {
   const isAdmin = localStorage.getItem("userRole") === "admin";
@@ -20,18 +27,13 @@ const Navbar = () => {
           showDrawer ? "block" : "hidden"
         } w-screen h-full absolute top-[100px] left-0 bg-light border-t-[1px] border-[#E5E5E5] shadow-lg z-20`}>
         <ul className="flex flex-col uppercase font-semibold text-xl divide-y-[1px] divide-[#EAEAEA] border-b-[1px] border-[#EAEAEA]">
-          <li className="py-4 px-10 cursor-pointer hover:underline hover:text-[#39523f]">
-            men
-          </li>
-          <li className="py-4 px-10 cursor-pointer hover:underline hover:text-[#39523f]">
-            women
-          </li>
-          <li className="py-4 px-10 cursor-pointer hover:underline hover:text-[#39523f]">
-            socks
-          </li>
-          <li className="py-4 px-10 cursor-pointer hover:underline hover:text-[#39523f]">
-            new arrivals
-          </li>
+          {navLinks.map((link) => (
+            <Link key={link.id} to={`products/${link.title}`}>
+              <li className="py-4 px-10 hover:underline hover:text-[#39523f]">
+                {link.title}
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
       <div className="wrapper flex-between shadow-md h-[60px] sticky top-0 bg-light z-10 py-2">
@@ -54,26 +56,29 @@ const Navbar = () => {
           <div className="relative w-full flex-between">
             <nav className="lg:w-1/3 hidden lg:block">
               <ul className="flex items-center justify-start gap-6 uppercase font-semibold text-sm">
-                <li className="cursor-pointer hover:underline hover:text-[#39523f]">
-                  men
-                </li>
-                <li className="cursor-pointer hover:underline hover:text-[#39523f]">
-                  women
-                </li>
-                <li className="cursor-pointer hover:underline hover:text-[#39523f]">
-                  socks
-                </li>
-                <li className="cursor-pointer hover:underline hover:text-[#39523f]">
-                  new arrivals
-                </li>
+                {navLinks.map((link) => (
+                  <Link key={link.id} to={`products/${link.title}`}>
+                    <li className="py-4 px-10 hover:underline hover:text-[#39523f]">
+                      {link.title}
+                    </li>
+                  </Link>
+                ))}
               </ul>
             </nav>
             <div className="lg:hidden w-1/3">
-              <IoIosMenu
-                size={30}
-                className="hover:text-[#39523f] cursor-pointer"
-                onClick={() => setShowDrawer(!showDrawer)}
-              />
+              {showDrawer ? (
+                <IoMdClose
+                  size={30}
+                  className="hover:text-[#39523f] cursor-pointer"
+                  onClick={() => setShowDrawer(!showDrawer)}
+                />
+              ) : (
+                <IoIosMenu
+                  size={30}
+                  className="hover:text-[#39523f] cursor-pointer"
+                  onClick={() => setShowDrawer(!showDrawer)}
+                />
+              )}
             </div>
 
             <div className="justify-self-center text-center">
