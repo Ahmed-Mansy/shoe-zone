1c1521357064ce1fa536908675ba3c03a296e0c9
AndrewEssam95 <andrewessam95@gmail.com>
2025-05-05 02:04:26 +0300

    small changes

--- frontend/sheozone_frontend/src/components/CollectionSidebar.jsx
+++ frontend/sheozone_frontend/src/components/CollectionSidebar.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const CollectionSidebar = ({ collectionTitle }) => {
   return (
-    <div className="w-1/4 bg-yellow-200 h-[200px]">
+    <div className="w-full bg-yellow-200 h-[200px]">
       <h2 className="text-2xl font-medium uppercase">{collectionTitle}</h2>
     </div>
   );
--- frontend/sheozone_frontend/src/components/ProductCard.jsx
+++ frontend/sheozone_frontend/src/components/ProductCard.jsx
@@ -1,5 +1,96 @@
+// import PropTypes from "prop-types";
+// import { Link } from "react-router";
+// import ImagesSlider from "./ImageSlider";
+
+// const ProductCard = ({ product, onDelete, onEdit }) => {
+//   const isAdmin = localStorage.getItem("userRole") === "admin";
+
+//   const { name, price, discount_price, images, id, available_colors } = product;
+//   // const navigate = useNavigate();
+
+//   const finalPrice = discount_price || price;
+
+//   return (
+//     <div className="w-full relative space-y-3 mx-2  my-4">
+//       {isAdmin ? (
+//         <div className="w-full aspect-square bg-[#f5f5f5] rounded-md overflow-hidden">
+//           {images.length > 0 ? (
+//             <ImagesSlider images={images} />
+//           ) : (
+//             <img
+//               src="/placeholder.jpg"
+//               alt={name}
+//               className="w-full h-full object-cover"
+//             />
+//           )}
+//         </div>
+//       ) : (
+//         <Link to={`/products/${id}`} state={{ product }}>
+//           <div className="w-full aspect-square bg-[#f5f5f5] rounded-md overflow-hidden">
+//             {images.length > 0 ? (
+//               <ImagesSlider images={images} />
+//             ) : (
+//               <img
+//                 src="/placeholder.jpg"
+//                 alt={name}
+//                 className="w-full h-full object-cover"
+//               />
+//             )}
+//           </div>
+//         </Link>
+//       )}
+
+//       <h3 className="text-md font-semibold mt-2 capitalize">{name}</h3>
+
+//       <div className="flex-start gap-1">
+//         {available_colors.map((color, index) => {
+//           return (
+//             <span
+//               key={index}
+//               style={{ backgroundColor: color }}
+//               className={`inline-block w-[25px] h-[25px] rounded-full border border-gray-900`}></span>
+//           );
+//         })}
+//       </div>
+
+//       <p className="text-md space-x-3">
+//         <span className="sm-semibold">{finalPrice} EGP</span>
+//         {discount_price && (
+//           <span className="text-gray-600 line-through">{price} EGP</span>
+//         )}
+//       </p>
+
+//       {isAdmin && (
+//         <div className="flex-center gap-2">
+//           <button
+//             onClick={() => onEdit(id)}
+//             className="bg-gray-500 text-white w-[80px] text-center cursor-pointer rounded-xs px-3 py-2 mx-2 hover:bg-gray-600 transition">
+//             Edit
+//           </button>
+
+//           <button
+//             onClick={() => onDelete(id)}
+//             className="bg-red-600 text-white w-[80px] text-center cursor-pointer rounded-xs px-3 py-2 hover:bg-red-700 transition">
+//             Delete
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// ProductCard.propTypes = {
+//   product: PropTypes.object.isRequired,
+//   onDelete: PropTypes.func.isRequired,
+//   onEdit: PropTypes.func.isRequired,
+// };
+
+// export default ProductCard;
 import PropTypes from "prop-types";
 import { Link } from "react-router";
+import ImagesSlider from "./ImagesSlider";
+
+import { FaStar, FaRegStar } from "react-icons/fa";
 
 const ProductCard = ({ product, onDelete, onEdit }) => {
   const isAdmin = localStorage.getItem("userRole") === "admin";
@@ -10,44 +101,61 @@ const ProductCard = ({ product, onDelete, onEdit }) => {
   const finalPrice = discount_price || price;
 
   return (
-    <div className="w-full relative space-y-3 mx-2  my-4">
+    <div className="w-full relative space-y-3 mx-2 my-4 border border-gray-300 rounded-md p-4 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out">
       <Link to={`/products/${id}`} state={{ product }}>
-        <div className="w-full aspect-square bg-[#f5f5f5]">
-          <img
-            src={`${isAdmin ? "" : "http://127.0.0.1:8000/"}${images[0].image}`}
-            alt={name}
-            className="w-full h-full"
-          /> */}
-          <img
-            src={`http://127.0.0.1:8000/${
-              images && images.length > 0 ? images[0].image : "default.jpg"
-            }`}
-            alt={name}
-            className="w-full h-full"
-          />
+        <div className="w-full aspect-square bg-[#f5f5f5] rounded-md overflow-hidden">
+          {images.length > 0 ? (
+            <ImagesSlider images={images} />
+          ) : (
+            <img
+              src="/placeholder.jpg"
+              alt={name}
+              className="w-full h-full object-cover"
+            />
+          )}
         </div>
       </Link>
 
-      <h3 className="text-md font-semibold mt-2 capitalize">{name}</h3>
-
-      <div className="flex-start gap-1">
-        {available_colors.map((color, index) => {
-          return (
-            <span
-              key={index}
-              style={{ backgroundColor: color }}
-              className={`inline-block w-[25px] h-[25px] rounded-full border border-gray-900`}
-            ></span>
+      <h3 className="text-md font-semibold mt-2 capitalize text-center">
+        {name}
+      </h3>
+      <div className="flex items-center gap-1 text-yellow-500 ">
+        {Array.from({ length: 5 }, (_, index) => {
+          const rating = Math.round(product.average_rating);
+          return index < rating ? (
+            <FaStar key={index} />
+          ) : (
+            <FaRegStar key={index} />
           );
         })}
       </div>
 
       <p className="text-md space-x-3">
+        {product.discount_price != 0 && (
+          <span className=" top-2 left-2 bg-red-500 text-white text-xs px-2 py-1 rounded">
+            Sale
+          </span>
+        )}
         <span className="sm-semibold">{finalPrice} EGP</span>
         {discount_price && (
           <span className="text-gray-600 line-through">{price} EGP</span>
         )}
       </p>
+
+      {!isAdmin && (
+        <Link to={`/products/${id}`} state={{ product }}>
+          <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+            Add to Cart
+          </button>
+        </Link>
+      )}
+
+      <span
+        className={`mx-4 my-4 ${
+          product.stock_quantity <= 0 ? "text-sm text-red-600 bold" : "hidden"
+        }`}>
+        Out of Stock
+      </span>
 
       {isAdmin && (
         <div className="flex-center gap-2">
@@ -59,8 +167,7 @@ const ProductCard = ({ product, onDelete, onEdit }) => {
 
           <button
             onClick={() => onDelete(id)}
-            className="bg-red-600 text-white w-[80px] text-center cursor-pointer rounded-xs px-3 py-2 hover:bg-red-700 transition"
-          >
+            className="bg-red-600 text-white w-[80px] text-center cursor-pointer rounded-xs px-3 py-2 hover:bg-red-700 transition">
             Delete
           </button>
         </div>
@@ -74,5 +181,4 @@ ProductCard.propTypes = {
   onDelete: PropTypes.func.isRequired,
   onEdit: PropTypes.func.isRequired,
 };
-
 export default ProductCard;
